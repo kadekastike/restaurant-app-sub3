315b7e649ff70edbc263f70aba9d31de12f3e881
Kadek Astike <kd.astike@gmail.com>
2023-03-09 21:31:37 +0700

    add db favorite db helper & provider

---
+++ lib/data/db/database_helper.dart
@@ -0,0 +1,81 @@
+import 'package:restaurant_app/data/model/restaurant_detail.dart';
+import 'package:restaurant_app/data/model/restaurant_list.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper? _instance;
+  static Database? _database;
+
+  DatabaseHelper._internal() {
+    _instance = this;
+  }
+
+  factory DatabaseHelper() => _instance ??DatabaseHelper._internal();
+
+  static const String _tblFavorite = 'favorites';
+
+  Future<Database> _initializeDb() async {
+    var path = await getDatabasesPath();
+    var db = openDatabase(
+      '$path/restaurantapp.db',
+      onCreate: (db, version) async {
+        await db.execute('''CREATE TABLE $_tblFavorite(
+          id TEXT PRIMARY KEY,
+          name TEXT,
+          city TEXT,
+          rating INT,
+          description TEXT
+          pictureId TEXT
+        )
+        ''');
+      },
+      version: 1
+    );
+
+    return db;
+  }
+
+  Future<Database?> get database async {
+    _database ??= await _initializeDb();
+
+    return _database;
+  }
+
+  Future<void> insertFavorite(RestaurantElement restaurantElement) async {
+    final db = await database;
+    await db!.insert(_tblFavorite, restaurantElement.toJson());
+  }
+
+  Future<List<RestaurantElement>> getFavorites() async {
+    final db = await database;
+    List<Map<String, dynamic>> results = await db!.query(_tblFavorite);
+
+    return results.map((res) => RestaurantElement.fromJson(res)).toList();
+  }
+
+  Future<Map> getFavoriteById(String id) async {
+    final db = await database;
+
+    List<Map<String, dynamic>> results = await db!.query(
+      _tblFavorite,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (results.isNotEmpty) {
+      return results.first;
+    } else {
+      return {};
+    }
+  }
+
+  Future<void> removeFavorite(String id) async {
+    final db = await database;
+
+    await db!.delete(
+      _tblFavorite,
+      where: 'url = ?',
+      whereArgs: [id],
+    );
+  }
+}
---
+++ lib/provider/database_provider.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/foundation.dart';
+import 'package:restaurant_app/common/result_state.dart';
+import 'package:restaurant_app/data/db/database_helper.dart';
+import 'package:restaurant_app/data/model/restaurant_list.dart';
+
+class DatabaseProvider extends ChangeNotifier {
+  final DatabaseHelper databaseHelper;
+
+  DatabaseProvider({ required this.databaseHelper}) {
+    _getFavorites();
+  }
+
+  late ResultState _state;
+  ResultState get state => _state;
+
+  String _message = '';
+  String get message => _message;
+
+  List<RestaurantElement> _favorites = [];
+  List<RestaurantElement> get favorites => _favorites;
+
+  void _getFavorites() async {
+    _favorites = await databaseHelper.getFavorites();
+    if (_favorites.isNotEmpty) {
+      _state = ResultState.hasData;
+    } else {
+      _state = ResultState.noData;
+      _message = 'Data Kosong';
+    }
+    notifyListeners();
+  }
+
+  void addFavorite(RestaurantElement restaurantElement) async {
+    try {
+      await databaseHelper.insertFavorite(restaurantElement);
+      _getFavorites();
+    } catch (e) {
+      _state = ResultState.error;
+      _message = 'Error: $e';
+      notifyListeners();
+    }
+  }
+
+  Future<bool> isFavorite(String id) async {
+    final favoritedRestaurant = await databaseHelper.getFavoriteById(id);
+    return favoritedRestaurant.isNotEmpty;
+  }
+
+  void removeFavorite(String id) async {
+    try {
+      await databaseHelper.removeFavorite(id);
+      _getFavorites();
+    } catch (e) {
+      _state = ResultState.error;
+      _message = 'Error: $e';
+      notifyListeners();
+    }
+  }
+}
